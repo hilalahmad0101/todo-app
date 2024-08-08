4a1dccf4e7aee823d35fe94eec34ee1739b34913
hilalahmad0101 <stackf63@gmail.com>
2024-08-08 09:45:08 +0500

    add file of data

--- config.js
+++
@@ -1,5 +0,0 @@
-export const apiBase = import.meta.env.VITE_API_BASE;
-
-const userString = localStorage.getItem("user");
-const user = JSON.parse(userString);
-export const userToken = user?.token;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
 import { Card, CardHeader, CardContent, TextField, Button } from '@mui/material';
-const Signup = () => {
+import { GlobalContext } from '../contextapi/GlobalContext';
+const Login = () => {
     let navigate = useNavigate();
+    const { setToken } = useContext(GlobalContext);
     const [users, setUsers] = useState({
-        name: '',
         email: '',
         password: ''
     });
     const inputHandle = (e) => {
-        e.preventDefault();
         let name = e.target.name;
         let value = e.target.value;
-
         setUsers({ ...users, [name]: value });
     }
-    const signup = async (e) => {
+    const login = async (e) => {
         e.preventDefault();
-        const { name, email, password } = users;
-        if (!name || !email || !password) {
-            alert('Please fill the Field');
+        const { email, password } = users;
+        if (!email || !password) {
+            alert('Please Fill the Field');
         } else {
             const data = {
-                name: name,
-                email: email,
-                password: password,
-            }
-            const res = await axios.post('http://localhost:5000/api/users/create', data);
+                email: email, password: password
+            };
+
+            const res = await axios.post('http://localhost:5000/api/users/login', data);
             if (res.data.status === 1) {
                 alert(res.data.message);
-                setUsers({ name: '', email: '', password: '' })
-                navigate('/login')
+                localStorage.setItem('token', res.data.jwt);
+                setToken(res.data.jwt);
+                setUsers({ email: '', password: '' });
+                navigate("/")
             } else {
                 alert(res.data.message);
             }
@@ -45,13 +45,13 @@ const Signup = () => {
                 <Grid container spacing={2} justifyContent="center">
                     <Grid item xl={6} lg={6} md={8} sm={8} xs={12}  >
                         <Card>
-                            <CardHeader title="Signup" />
+                            <CardHeader title="Login" />
                             <CardContent>
-                                <form action="" onSubmit={signup}>
-                                    <TextField name="name" onChange={inputHandle} value={users.name} className="w-100" type="text" label="Enter your Name" variant="standard"></TextField>
-                                    <TextField name="email" onChange={inputHandle} value={users.email} className="w-100  my-3" type="email" label="Enter your Email" variant="standard"></TextField>
+                                <form action="" onSubmit={login}>
+
+                                    <TextField name="email" onChange={inputHandle} value={users.email} className="w-100  mb-3" type="email" label="Enter your Email" variant="standard"></TextField>
                                     <TextField name="password" onChange={inputHandle} value={users.password} className="w-100" type="password" label="Enter your Password" variant="standard"></TextField>
-                                    <Button type="submit" color="primary" className="w-25 my-4" variant="contained">Signup</Button>
+                                    <Button type="submit" color="primary" className="w-25 my-4" variant="contained">Login</Button>
                                 </form>
                             </CardContent>
                         </Card>
@@ -62,4 +62,4 @@ const Signup = () => {
     );
 };
 
-export default Signup;
+export default Login;
